701c75da8c136fc9df85aee827f3db6500731ace
Mamoutova <olga.mamoutova@gmail.com>
2014-02-09 20:21:31 +0400

    major fix of interfaces for nios
    to work with niosII /f on avalon bus
    read signal instead of chipselect signal
    fi_ack logic tweaked a little bit accordingly

--- hdl/fi_mem_agent.vhd
+++ hdl/fi_mem_agent.vhd
@@ -37,6 +37,10 @@
 -- Modification date: 12 Dec 2013
 -- Notes: generic USE_ACK - whether to use ack or two-cycle read/one-cycle write
 -- Limitation:
+-- Revision:  6.0
+-- Modification date: 09 Feb 2013
+-- Notes: implement rd signal instead of cs + code compactness + tweaked fi_ack logic
+-- Limitation:
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
@@ -47,37 +51,37 @@ ENTITY fi_mem_agent IS
 	GENERIC
 	(
 		use_ack		: BOOLEAN := true; 		-- use_ack = 0 - two-cycle read/one-cycle write, use_ack = 1 - to use ack
-		aw			: INTEGER := 32;
-		dw			: INTEGER := 32
+		aw				: INTEGER := 32;
+		dw				: INTEGER := 32
 	);
 	PORT
 	( 
-		clk 		: IN 		STD_LOGIC;
+		clk 			: IN 		STD_LOGIC;
 		rst_n 		: IN 		STD_LOGIC;
 		
 		-- fault injection command
-		fi_ena		: IN 		STD_LOGIC;
-		fi_wr   	: IN 		STD_LOGIC;
-		fi_A 		: IN 		STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
+		fi_rd			: IN 		STD_LOGIC;
+		fi_wr   		: IN 		STD_LOGIC;
+		fi_A 			: IN 		STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
 		fi_Mask		: IN 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
-		fi_data_r 	: OUT 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
+		fi_data_r 	: OUT 	STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
 		fi_ack 		: OUT		STD_LOGIC;		-- active high = waitrequest_n
 		
 		-- Original memory interface
-		CE			: IN 		STD_LOGIC;
-		WE			: IN 		STD_LOGIC;
-		A			: IN		STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
+		RD				: IN 		STD_LOGIC;
+		WE				: IN 		STD_LOGIC;
+		A				: IN		STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
 		ACK			: OUT		STD_LOGIC;		-- active high = waitrequest_n
-		D			: IN 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
-		Dout		: OUT 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
+		D				: IN 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
+		Dout			: OUT 	STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
 		
 		-- Modified memory interface
-		CE_m		: OUT 	STD_LOGIC;
-		WE_m		: OUT 	STD_LOGIC;
-		ACK_m		: IN 	STD_LOGIC;			-- active high = waitrequest_n
+		RD_m			: OUT 	STD_LOGIC;
+		WE_m			: OUT 	STD_LOGIC;
+		ACK_m			: IN 		STD_LOGIC;			-- active high = waitrequest_n
 		A_m			: OUT 	STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
 		D_m			: OUT 	STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
-		Dout_m		: IN 	STD_LOGIC_VECTOR(dw-1 DOWNTO 0)	
+		Dout_m		: IN 		STD_LOGIC_VECTOR(dw-1 DOWNTO 0)	
 	);
 END fi_mem_agent;
 
@@ -91,7 +95,7 @@ ARCHITECTURE rtl OF fi_mem_agent IS
 	);
 	SIGNAL fsm 	: fi_state_type;
 
-	SIGNAL fi_Mask_reg 		: STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
+	SIGNAL fi_Mask_reg 	: STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
 	SIGNAL fi_A_reg 		: STD_LOGIC_VECTOR(aw-1 DOWNTO 0);
 	SIGNAL Dout_m_reg		: STD_LOGIC_VECTOR(dw-1 DOWNTO 0);
 	
@@ -99,13 +103,13 @@ ARCHITECTURE rtl OF fi_mem_agent IS
 		
 BEGIN
 
-	fi_a_reg_p: PROCESS(clk, rst_n, A, fi_ena, fi_Mask)
+	fi_a_reg_p: PROCESS(clk, rst_n, A, fi_wr, fi_rd, fi_Mask)
 	BEGIN
 		IF rst_n = '0' THEN
 			fi_A_reg <= (OTHERS=>'0');
 			fi_Mask_reg <= (OTHERS=>'0');
 		ELSIF clk'event AND clk = '1' THEN
-			IF (fi_ena = '1') THEN
+			IF (fi_wr = '1' OR fi_rd = '1') THEN
 				fi_A_reg <= fi_A(aw-1 DOWNTO 0);
 				IF fi_wr = '1' THEN
 					fi_Mask_reg <= fi_Mask;
@@ -117,21 +121,19 @@ BEGIN
 USE_ACK_GEN: IF use_ack GENERATE
  
 	-- fsm transitions
-	fsm_transitions: PROCESS(clk, rst_n, A, fi_ena)
+	fsm_transitions: PROCESS(clk, rst_n, A, fi_rd, fi_wr)
 	BEGIN		
 		IF rst_n = '0' THEN
 			fsm <= idle;							
 			Dout_m_reg <= (OTHERS=>'0');
 		ELSIF clk'event AND clk = '1' THEN
---			fsm <= fsm;						-- !!!!!
+			fsm <= fsm;
 			CASE fsm IS
 				WHEN idle =>
-					IF (fi_ena = '1') THEN						
-						IF fi_wr = '1' THEN
-							fsm <= fi_read_before_write;
-						ELSE
-							fsm <= fi_read;
-						END IF;
+					IF (fi_wr = '1') THEN
+						fsm <= fi_read_before_write;
+					ELSIF (fi_rd = '1') THEN
+						fsm <= fi_read;
 					ELSE
 						fsm  <= idle;
 					END IF;
@@ -162,27 +164,11 @@ USE_ACK_GEN: IF use_ack GENERATE
 	
 	ACK <= ACK_m WHEN fsm=idle ELSE '0';
 	
-	-- fsm outputs
+	-- fi_ack logic
 	PROCESS(fsm, D, Dout_m_reg, fi_Mask_reg, WE, rst_n, ACK_m)
 	BEGIN
-		IF fsm = idle THEN
-			D_m <= D;
-		ELSE
-			D_m <= Dout_m_reg XOR fi_Mask_reg;
-		END IF;
-		
-		IF fsm = idle THEN
-			WE_m <= WE;
-		ELSIF fsm = fi_write THEN
-			WE_m <= '1';
-		ELSE
-			WE_m <= '0';
-		END IF;
-		
 		IF rst_n = '0' THEN
 			fi_ack_int <= '0';
-		ELSIF fsm = idle THEN
-			fi_ack_int <= '1';
 		ELSIF fsm = fi_write THEN
 			fi_ack_int <= ACK_m;
 		ELSIF fsm = fi_read THEN
@@ -196,22 +182,22 @@ USE_ACK_GEN: IF use_ack GENERATE
 END GENERATE USE_ACK_GEN;
 NOT_USE_ACK_GEN: IF NOT use_ack GENERATE
 
+	ACK <= '1';
+
 	-- fsm transitions
-	fsm_transitions: PROCESS(clk, rst_n, A, fi_ena)
+	fsm_transitions: PROCESS(clk, rst_n, A, fi_rd, fi_wr)
 	BEGIN		
 		IF rst_n = '0' THEN
 			fsm <= idle;	
 			Dout_m_reg <= (OTHERS=>'0');
 		ELSIF clk'event AND clk = '1' THEN
-			fsm <= fsm;						-- !!!!!
+			fsm <= fsm;
 			CASE fsm IS
 				WHEN idle =>
-					IF (fi_ena = '1') THEN
-						IF fi_wr = '1' THEN
-							fsm <= fi_read_before_write;
-						ELSE
-							fsm <= fi_read;
-						END IF;
+					IF (fi_wr = '1') THEN
+						fsm <= fi_read_before_write;
+					ELSIF (fi_rd = '1') THEN
+						fsm <= fi_read;
 					ELSE
 						fsm  <= idle;
 					END IF;
@@ -232,29 +218,11 @@ NOT_USE_ACK_GEN: IF NOT use_ack GENERATE
 		END IF;	
 	END PROCESS;	
 
-	ACK <= '1';
-
-	-- fsm outputs
+	-- fi_ack logic
 	PROCESS(fsm, D, Dout_m_reg, fi_Mask_reg, WE, rst_n)
 	BEGIN
-		IF fsm = idle THEN
-			D_m <= D;
-		ELSE
-			D_m <= Dout_m_reg XOR fi_Mask_reg;
-		END IF;
-		
-		IF fsm = idle THEN
-			WE_m <= WE;
-		ELSIF fsm = fi_write THEN
-			WE_m <= '1';
-		ELSE
-			WE_m <= '0';
-		END IF;
-		
 		IF rst_n = '0' THEN
 			fi_ack_int <= '0';
-		ELSIF fsm = idle THEN
-			fi_ack_int <= '1';
 		ELSIF fsm = fi_write THEN
 			fi_ack_int <= '1';
 		ELSIF fsm = fi_read1 THEN
@@ -266,25 +234,26 @@ NOT_USE_ACK_GEN: IF NOT use_ack GENERATE
 
 END GENERATE NOT_USE_ACK_GEN;
 	
-	PROCESS(rst_n, clk, fi_ack_int)
-	BEGIN
-		IF rst_n = '0' THEN
-			fi_ack_int_td  <= '0';
-		ELSIF clk'event AND clk = '1' THEN
-			fi_ack_int_td <= fi_ack_int;
-		END IF;
-	END PROCESS;
+	fi_ack <= fi_ack_int;
 	
-	fi_ack <= ((NOT fi_ena) AND fi_ack_int) OR (fi_ena AND fi_ack_int AND (NOT fi_ack_int_td));
-	
-	PROCESS(fsm, fi_A_reg, CE, A)
+	-- common fsm outputs
+	PROCESS(fsm, fi_A_reg, RD, A)
 	BEGIN
 		IF (fsm /= idle) THEN
-			CE_m <= '1';
+			IF fsm = fi_write THEN
+				WE_m <= '1';
+				RD_m <= '0';
+			ELSE
+				WE_m <= '0';
+				RD_m <= '1';				
+			END IF;
 			A_m <= fi_A_reg;
+			D_m <= Dout_m_reg XOR fi_Mask_reg;			
 		ELSE
-			CE_m <= CE;
+			RD_m <= RD;
+			WE_m <= WE;
 			A_m <= A;
+			D_m <= D;
 		END IF;
 	END PROCESS;
 	
